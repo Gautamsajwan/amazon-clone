1ac977cd991d38aaee38e99f3ccc49d70b6437d8
gautam <gautamsajwan8126@gmail.com>
2023-07-14 21:25:54 +0530

    deployed successfully

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,19 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCVk-484zHGetLu7wP4tFeqMNRIofF8zw",
-    authDomain: "challenge-25608.firebaseapp.com",
-    projectId: "challenge-25608",
-    storageBucket: "challenge-25608.appspot.com",
-    messagingSenderId: "943303587826",
-    appId: "1:943303587826:web:83463a8347eb246d48dd6b"
-  };
+  apiKey: "AIzaSyDxXNW83RWkcnIHjNPGOh2afnNzhgE5WsM",
+  authDomain: "clone2-32839.firebaseapp.com",
+  projectId: "clone2-32839",
+  storageBucket: "clone2-32839.appspot.com",
+  messagingSenderId: "481538568746",
+  appId: "1:481538568746:web:dfebc95f2495ae89f5c953"
+};
 
-  // initialize the firebase config
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+// initialize the firebase config
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  // initialize the database
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
+// initialize the database
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-  export { db, auth }; // exporting these will allow us to use database and authentatication outside of the file
+export { db, auth }; // exporting these will allow us to use database and authentatication outside of the file
